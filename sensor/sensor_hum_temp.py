37e14efe08397e135025f8eb49fb0b90c3d5267f
alisa6437 <150240759+alisa6437@users.noreply.github.com>
2025-05-25 21:54:54 +0200

    Create sensor_hum_temp.py

---
+++ sensor/sensor_hum_temp.py
@@ -0,0 +1,28 @@
+import time
+import adafruit_dht
+import paho.mqtt.client as mqtt
+
+MQTT_BROKER = "localhost"
+MQTT_TOPIC = "greenhouse/temp"
+
+sensor = adafruit_dht.DHT11(board.D22)
+
+client = mqtt.Client()
+client.connect(MQTT_BROKER)
+
+try:
+    while True:
+        try:
+            temperature = sensor.temperature
+            humidity = sensor.humidity
+            print(f"Temp: {temperature}°C, Humidity: {humidity}%")
+
+            # send temperature as string
+            client.publish(MQTT_TOPIC, str(temperature))
+        except RuntimeError as e:
+            print(f"DHT read error: {e.args[0]}")
+        time.sleep(3)
+
+except KeyboardInterrupt:
+    sensor.exit()
+    client.disconnect()
