41367ffb598da20747ea27f5e77a56463232da69
alisa6437 <150240759+alisa6437@users.noreply.github.com>
2025-05-25 14:05:52 +0200

    Update sensor_light.py

--- sensor/sensor_light.py
+++ sensor/sensor_light.py
@@ -1,1 +1,32 @@
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+import time
+
+LDR_PIN = 6
+MQTT_BROKER = "192.168.2.138"  
+MQTT_TOPIC = "greenhouse/light"
+
+def rc_time(pin):
+    reading = 0
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, False)
+    time.sleep(0.1)
+    GPIO.setup(pin, GPIO.IN)
+    while GPIO.input(pin) == GPIO.LOW:
+        reading += 1
+    return reading
+
+GPIO.setmode(GPIO.BCM)
+client = mqtt.Client()
+client.connect(MQTT_BROKER)
+
+try:
+    while True:
+        value = rc_time(LDR_PIN)
+        print("Send light value:", value)
+        client.publish(MQTT_TOPIC, value)
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    client.disconnect()
 
